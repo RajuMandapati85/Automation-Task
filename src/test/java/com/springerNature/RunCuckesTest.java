b2bf0f086851afd99055c092be0eb2c3a4004a61
Sheebashanthi <Sheebashanthi@gmail.com>
2020-01-08 22:04:03 +0000

    Test

--- src/test/java/com/springerNature/RunCuckesTest.java
+++ src/test/java/com/springerNature/RunCuckesTest.java
@@ -5,17 +5,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-/**
- * Created by bothees on 27/11/2018.
- */
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
                  format ={"html:target/cucumber-html-report", "json:target/cucumber.json"},
                  tags = {"@test"},
                  features ="src/test/resources/features",
-                 glue = {"com.blackcat.utilities",
-                         "com.blackcat.stepdefinitions"})
+                 glue = {"com.springerNature.utilities",
+                         "com.springerNature.stepdefinitions"})
 
 public class RunCuckesTest extends Setup {
 
